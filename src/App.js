83d2c039260e10d47783e588be6690692dc38c90
Hibiki <hibiki.tsuboi@gmail.com>
2021-12-06 22:26:38 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
+import React from 'react';
+
 function App() {
-  return <h1>Hello</h1>;
+  // const greeting = "Hi!"
+  // const dom = <h1 className="foo">{greeting}</h1>;
+  // return dom;
+
+  // return <input type="text" onClick={() => {
+  //   console.log("I am clicked.")
+  // }}/>
+
+  // return <input type="text" onChange={() => {
+  //   console.log("I am clicked.")
+  // }}/>
+
+  return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {
+        console.log("I am clicked.")
+      }}/>
+    </React.Fragment>
+  )
 }
+
 export default App;
