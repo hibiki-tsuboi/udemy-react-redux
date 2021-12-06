76bad600566dad2a5c48c35dce095f6f36ad4664
Hibiki <hibiki.tsuboi@gmail.com>
2021-12-06 22:50:51 +0900

    create component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
 
-function App() {
-  // const greeting = "Hi!"
-  // const dom = <h1 className="foo">{greeting}</h1>;
-  // return dom;
+// function App() {
+// const greeting = "Hi!"
+// const dom = <h1 className="foo">{greeting}</h1>;
+// return dom;
 
-  // return <input type="text" onClick={() => {
-  //   console.log("I am clicked.")
-  // }}/>
+// return <input type="text" onClick={() => {
+//   console.log("I am clicked.")
+// }}/>
 
-  // return <input type="text" onChange={() => {
-  //   console.log("I am clicked.")
-  // }}/>
+// return <input type="text" onChange={() => {
+//   console.log("I am clicked.")
+// }}/>
 
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {
+//         console.log("I am clicked.")
+//       }}/>
+//     </React.Fragment>
+//   )
+// }
+
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {
-        console.log("I am clicked.")
-      }}/>
-    </React.Fragment>
+    <div>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+      <Cat/>
+    </div>
   )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
