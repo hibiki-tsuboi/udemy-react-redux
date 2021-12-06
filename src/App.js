dfdf6bd8ec85c5e759eaeb3d654614be500a658d
Hibiki <hibiki.tsuboi@gmail.com>
2021-12-06 23:56:50 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,60 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {Component} from 'react';
 
-// function App() {
-// const greeting = "Hi!"
-// const dom = <h1 className="foo">{greeting}</h1>;
-// return dom;
+const App = () => (<Counter></Counter>)
 
-// return <input type="text" onClick={() => {
-//   console.log("I am clicked.")
-// }}/>
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    this.state = {count: 0}
+  }
 
-// return <input type="text" onChange={() => {
-//   console.log("I am clicked.")
-// }}/>
+  handlePlusbutton = () => {
+    console.log("handlePlusbutton")
+    console.log(this.state.count)
+    this.setState({count: this.state.count + 1})
+  }
 
-//   return (
-//     <React.Fragment>
-//       <label htmlFor="bar">bar</label>
-//       <input type="text" onChange={() => {
-//         console.log("I am clicked.")
-//       }}/>
-//     </React.Fragment>
-//   )
-// }
+  handleMinusbutton = () => {
+    this.setState({count: this.state.count - 1})
+  }
 
-const App = () => {
-  const profiles = [
-    {name: "Taro", age: 10},
-    {name: "Hanako", age: 2},
-    {name: "Jiro", age: 2},
-  ]
-
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index}/>
-        })
-      }
-    </div>
-  )
+  render() {
+    return <>
+      <div>count: {this.state.count}</div>
+      <button onClick={this.handlePlusbutton}>+1</button>
+      <button onClick={this.handleMinusbutton}>-1</button>
+    </>
+  }
 }
 
-// const Cat = () => {
-//   return <div>Meow!</div>
-// }
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old</div>
-}
-
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired,
-}
-
-// User.defaultProps = {
-//   age: 1
-// }
 export default App;
